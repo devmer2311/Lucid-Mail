ab72bb46e45c192e82444be166d7d1b37f746226
Dev Mer <138443145+devmer2311@users.noreply.github.com>
2025-09-03 23:14:14 +0530

    Update main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -2,13 +2,27 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // Next.js dev frontend
+      'https://your-frontend-domain.com', 
+    ],
+    credentials: true,
+  });
+
+  
   app.setGlobalPrefix('api');
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
   console.log(`🚀 Backend running at http://localhost:${port}/api`);
 }
 bootstrap();
